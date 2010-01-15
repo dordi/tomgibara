0030a6d7c19fa7dbaac48e3e8684c7a7c0ef2253
tomgibara <tomgibara@90b63902-e2c4-11de-9097-f71ab900b11a>
2010-01-15 22:17:09 +0000

    Moved index annotation methods off StuppType and onto StuppIndex.

--- stupp/src/main/java/com/tomgibara/stupp/StuppIndex.java
+++ stupp/src/main/java/com/tomgibara/stupp/StuppIndex.java
@@ -16,6 +16,11 @@
  */
 package com.tomgibara.stupp;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
@@ -24,11 +29,85 @@ import java.util.regex.Pattern;
 //TODO should throw meaningful exception when not in scope
 public abstract class StuppIndex<C> {
 
+	// statics
+	
 	private static final Pattern validNamePattern = Pattern.compile("[A-Za-z]+");
+	
+	private static final HashMap<Class<? extends Annotation>, Constructor<? extends StuppIndex<?>>> indexCons = new HashMap<Class<? extends Annotation>, Constructor<? extends StuppIndex<?>>>();
 	
 	static void checkName(String name) {
 		if (!validNamePattern.matcher(name).matches()) throw new IllegalArgumentException("Index name " + name + "does not match pattern: " + validNamePattern.pattern());
 	}
+	
+	//this can probably avoid being exposed unless classloader situations arise
+	//note: due to the large number of checks peformed by this method, callers should first confirm that the annotationType has not already been registered
+	static void registerIndexAnnotation(Class<? extends Annotation> annotationType) {
+		//null check
+		if (annotationType == null) throw new IllegalArgumentException("null annotationType");
+		final StuppIndexDefinition definition = annotationType.getAnnotation(StuppIndexDefinition.class);
+		//no index class defined
+		if (definition == null) throw new IllegalArgumentException("No StuppIndexDefinition for annotation: " + annotationType);
+		//does the annotation have a name method?
+		try {
+			Method method = annotationType.getMethod("name", Stupp.NO_PARAMS);
+			if (method.getReturnType() != String.class) throw new IllegalArgumentException("Annotation has name property which is not a String: " + method.getReturnType());
+		} catch (NoSuchMethodException e) {
+			throw new IllegalArgumentException("Annotation has no name property.");
+		}
+		final Class<? extends StuppIndex<?>> indexClass = definition.value();
+		if (indexClass == null) throw new IllegalArgumentException("null indexClass");
+		//can't trust generics - is it an index?
+		if (!StuppIndex.class.isAssignableFrom(indexClass)) throw new IllegalArgumentException("Index class does not extend StuppIndex: " + indexClass);
+		//is it concrete?
+		if (Modifier.isAbstract(indexClass.getModifiers())) throw new IllegalArgumentException("Index class is abstract: " + indexClass);
+		//does it have a relevant constructor?
+		final Constructor<? extends StuppIndex<?>> constructor;
+		try {
+			constructor = indexClass.getConstructor(StuppProperties.class, annotationType);
+		} catch (NoSuchMethodException e) {
+			throw new IllegalArgumentException("Index has no (StuppProperties, Annotation) constructor: " + indexClass);
+		}
+		//all checks done, now register
+		synchronized (indexCons) {
+			// no need to check for duplication, call is idempotent
+			indexCons.put(annotationType, constructor);
+		}
+	}
+	
+	static String checkForIndexAnnotation(Annotation annotation) {
+		final Class<? extends Annotation> annotationType = annotation.annotationType();
+		for (Annotation ann : annotationType.getAnnotations()) {
+			if (ann.annotationType() == StuppIndexDefinition.class) {
+				final boolean alreadyRegistered;
+				synchronized(indexCons) {
+					alreadyRegistered = indexCons.containsKey(annotationType);
+				}
+				if (!alreadyRegistered) registerIndexAnnotation(annotationType);
+				return Stupp.nameOfAnnotation(annotation);
+			}
+		}
+		return null;
+	}
+
+	static StuppIndex<?> createIndex(StuppProperties properties, Annotation definition) {
+		synchronized (indexCons) {
+			Constructor<? extends StuppIndex<?>> constructor = indexCons.get(definition.annotationType());
+			if (constructor == null) throw new IllegalStateException("No constructor for index definition annotation: " + definition);
+			try {
+				return constructor.newInstance(properties, definition);
+			} catch (IllegalArgumentException e) {
+				throw new IllegalStateException(e);
+			} catch (InstantiationException e) {
+				throw new RuntimeException(e);
+			} catch (IllegalAccessException e) {
+				throw new RuntimeException(e);
+			} catch (InvocationTargetException e) {
+				throw new RuntimeException(e);
+			}
+		}
+	}
+	
+	// fields
 	
 	final StuppProperties properties;
 	final String name;

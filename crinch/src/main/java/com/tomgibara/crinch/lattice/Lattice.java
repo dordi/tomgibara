280db8a411bda722d6e3bb7904dd2be1428718e8
tomgibara <tomgibara@90b63902-e2c4-11de-9097-f71ab900b11a>
2010-03-17 23:40:11 +0000

    Completely untested product lattice.

--- crinch/src/main/java/com/tomgibara/crinch/lattice/Lattice.java
+++ crinch/src/main/java/com/tomgibara/crinch/lattice/Lattice.java
@@ -1,5 +1,6 @@
 package com.tomgibara.crinch.lattice;
 
+//TODO can optimize implementation of bounded methods by checking for equality with top/bottom
 public interface Lattice<E> extends MeetSemiLattice<E>, JoinSemiLattice<E> {
 
 	Lattice<E> boundedAbove(E top);

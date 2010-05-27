7047a65ff3d8cf94bb7400ec7a220da4f4330319
tomgibara <tomgibara@90b63902-e2c4-11de-9097-f71ab900b11a>
2010-05-27 01:06:51 +0000

    Added methods to scan for bit positions.

--- crinch/src/test/java/com/tomgibara/crinch/bits/BitVectorTest.java
+++ crinch/src/test/java/com/tomgibara/crinch/bits/BitVectorTest.java
@@ -623,6 +623,45 @@ public class BitVectorTest extends TestCase {
 		assertEquals(v, w);
 	}
 
+	public void testFirstInRange() {
+		for (int i = 0; i < 1000; i++) {
+			BitVector v = new BitVector(1000);
+			int vSize = v.size();
+			int a = random.nextInt(vSize+1);
+			int b = a + random.nextInt(vSize+1-a);
+			BitVector w = v.rangeView(a, b);
+			int c;
+			int wSize = w.size();
+			if (wSize == 0) {
+				c = -1;
+			} else {
+				c = random.nextInt(wSize);
+				w.setBit(c, true);
+			}
+
+			assertEquals(c, w.firstOne());
+			assertEquals(c, w.lastOne());
+			if (c >= 0) {
+				assertEquals(c, w.nextOne(c));
+				assertEquals(-1, w.previousOne(c));
+				if (c > 0) assertEquals(c, w.nextOne(c-1));
+				if (c < wSize) assertEquals(c, w.previousOne(c+1));
+				assertEquals(c, w.nextOne(0));
+				assertEquals(c, w.previousOne(wSize));
+			}
+			w.flip();
+			assertEquals(c, w.firstZero());
+			assertEquals(c, w.lastZero());
+			if (c >= 0) {
+				assertEquals(c, w.nextZero(c));
+				assertEquals(-1, w.previousZero(c));
+				if (c > 0) assertEquals(c, w.nextZero(c-1));
+				if (c < wSize) assertEquals(c, w.previousZero(c+1));
+				assertEquals(c, w.nextZero(0));
+				assertEquals(c, w.previousZero(wSize));
+			}
+		}
+	}
 	
 	
 }

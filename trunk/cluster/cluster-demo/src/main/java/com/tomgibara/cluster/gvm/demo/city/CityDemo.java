29d5cb72f220b8dbe6f4ddcf670d077a73bf7d0d
tomgibara <tomgibara@90b63902-e2c4-11de-9097-f71ab900b11a>
2011-04-14 15:55:23 +0000

    Added licence notices to source files.

--- trunk/cluster/cluster-demo/src/main/java/com/tomgibara/cluster/gvm/demo/city/CityDemo.java
+++ trunk/cluster/cluster-demo/src/main/java/com/tomgibara/cluster/gvm/demo/city/CityDemo.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2007 Tom Gibara
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
 package com.tomgibara.cluster.gvm.demo.city;
 import java.awt.BorderLayout;
 import java.awt.Color;
@@ -5,11 +21,6 @@ import java.awt.Component;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
@@ -20,7 +31,6 @@ import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.List;
 import java.util.Map;
-import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -33,8 +43,8 @@ import javax.swing.JSlider;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
-import com.tomgibara.cluster.gvm.dbl.DblListKeyer;
 import com.tomgibara.cluster.gvm.dbl.DblClusters;
+import com.tomgibara.cluster.gvm.dbl.DblListKeyer;
 import com.tomgibara.cluster.gvm.dbl.DblResult;
 
 /*

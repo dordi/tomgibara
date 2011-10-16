6c9a0c96f69c572c957388285843304a309cf169
tomgibara <tomgibara@90b63902-e2c4-11de-9097-f71ab900b11a>
2011-10-16 22:16:12 +0000

    Added transpose constructor.

--- trunk/crinch/src/test/java/com/tomgibara/crinch/perm/PermutationTest.java
+++ trunk/crinch/src/test/java/com/tomgibara/crinch/perm/PermutationTest.java
@@ -3,6 +3,7 @@ package com.tomgibara.crinch.perm;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 import com.tomgibara.crinch.perm.permutable.PermutableList;
 
@@ -69,7 +70,21 @@ public class PermutationTest extends TestCase {
 			}
 		}
 	}
-	
+
+	public void testTransposeConstructor() {
+		Random r = new Random(0L);
+		for (int n = 0; n < 10000; n++) {
+			int size = r.nextInt(10) + 2;
+			int i = r.nextInt(size);
+			int j = r.nextInt(size);
+			Permutation p = Permutation.transpose(size, i, j);
+			assertEquals(i == j, p.getInfo().isIdentity());
+			assertEquals(p, p.generator().invert().permutation());
+			assertEquals(i == j ? 0 : 1, p.getInfo().getNumberOfTranspositions());
+			assertEquals(i == j ? 0 : 1, p.getInfo().getNumberOfCycles());
+		}
+	}
+
 	public void testCorrespondenceConstructor() {
 		verifyBadConstructor(0,1,2,3,3);
 		verifyBadConstructor(1);

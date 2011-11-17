3c6304f2fa7accd43e87f2411d4eb52816b5be96
tomgibara <tomgibara@90b63902-e2c4-11de-9097-f71ab900b11a>
2011-11-17 07:54:14 +0000

    Initial implementation of record position indexing.

---
+++ trunk/crinch/crinch-record/src/main/java/com/tomgibara/crinch/record/compact/PositionProducer.java
@@ -0,0 +1,16 @@
+package com.tomgibara.crinch.record.compact;
+
+import com.tomgibara.crinch.record.LinearRecord;
+import com.tomgibara.crinch.record.ProcessContext;
+import com.tomgibara.crinch.record.RecordProducer;
+import com.tomgibara.crinch.record.RecordSequence;
+
+public class PositionProducer implements RecordProducer<LinearRecord> {
+
+	@Override
+	public RecordSequence<LinearRecord> open(ProcessContext context) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

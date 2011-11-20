3977f6e8f41f4b3c32181a4123713aebc696a414
tomgibara <tomgibara@90b63902-e2c4-11de-9097-f71ab900b11a>
2011-11-20 00:22:07 +0000

    Position indexing now accurately calculates best fixed size.

--- crinch/crinch-record/src/main/java/com/tomgibara/crinch/record/compact/PositionProducer.java
+++ crinch/crinch-record/src/main/java/com/tomgibara/crinch/record/compact/PositionProducer.java
@@ -1,16 +1,54 @@
 package com.tomgibara.crinch.record.compact;
 
-import com.tomgibara.crinch.record.LinearRecord;
+import com.tomgibara.crinch.record.EmptyRecord;
 import com.tomgibara.crinch.record.ProcessContext;
 import com.tomgibara.crinch.record.RecordProducer;
 import com.tomgibara.crinch.record.RecordSequence;
 
-public class PositionProducer implements RecordProducer<LinearRecord> {
+public class PositionProducer implements RecordProducer<EmptyRecord> {
 
 	@Override
-	public RecordSequence<LinearRecord> open(ProcessContext context) {
-		// TODO Auto-generated method stub
-		return null;
+	public void prepare(ProcessContext context) {
+	}
+	
+	@Override
+	public RecordSequence<EmptyRecord> open() {
+		return new PositionSequence();
+	}
+
+	@Override
+	public void complete() {
+	}
+	
+	private class PositionSequence implements RecordSequence<EmptyRecord> {
+
+		PositionSequence() {
+		}
+		
+		@Override
+		public boolean hasNext() {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public EmptyRecord next() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public void remove() {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void close() {
+			// TODO Auto-generated method stub
+			
+		}
+		
 	}
 
 }

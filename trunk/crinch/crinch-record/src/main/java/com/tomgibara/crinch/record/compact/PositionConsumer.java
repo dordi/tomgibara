be477b6e71c61bdea74931c70a3c247e60221692
tomgibara <tomgibara@90b63902-e2c4-11de-9097-f71ab900b11a>
2011-11-28 09:57:16 +0000

    Trapping records without either position or ordinal during consumption.

--- trunk/crinch/crinch-record/src/main/java/com/tomgibara/crinch/record/compact/PositionConsumer.java
+++ trunk/crinch/crinch-record/src/main/java/com/tomgibara/crinch/record/compact/PositionConsumer.java
@@ -54,6 +54,7 @@ public class PositionConsumer implements RecordConsumer<LinearRecord> {
 
 	@Override
 	public void beginPass() {
+		context.setPassName("Building position index");
 		//TODO support larger counts
 		int count = (int) recStats.getRecordCount();
 		positions = new long[count];
@@ -61,7 +62,12 @@ public class PositionConsumer implements RecordConsumer<LinearRecord> {
 
 	@Override
 	public void consume(LinearRecord record) {
-		positions[(int) record.getRecordOrdinal()] = record.getRecordPosition();
+		long position = record.getRecordPosition();
+		int ordinal = (int) record.getRecordOrdinal();
+		//TODO could deal with this by maintaining own count of records and decrementing
+		if (position < 0L) throw new IllegalArgumentException("record without position");
+		if (ordinal < 0) throw new IllegalArgumentException("record without ordinal");
+		positions[ordinal] = position;
 		record.exhaust();
 	}
 

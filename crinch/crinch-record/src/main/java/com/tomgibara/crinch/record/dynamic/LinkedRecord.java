3ca503a679fdf19e21f5302acc1b8555a97b4273
tomgibara <tomgibara@90b63902-e2c4-11de-9097-f71ab900b11a>
2011-11-28 00:39:42 +0000

    Added configuration options to dynamic record instantiation that supports linked lists of records.

---
+++ crinch/crinch-record/src/main/java/com/tomgibara/crinch/record/dynamic/LinkedRecord.java
@@ -0,0 +1,17 @@
+package com.tomgibara.crinch.record.dynamic;
+
+import com.tomgibara.crinch.record.LinearRecord;
+
+public interface LinkedRecord extends LinearRecord {
+
+	void insertRecordBefore(LinkedRecord record);
+	
+	void insertRecordAfter(LinkedRecord record);
+	
+	LinkedRecord getNextRecord();
+	
+	LinkedRecord getPreviousRecord();
+	
+	void removeRecord();
+	
+}

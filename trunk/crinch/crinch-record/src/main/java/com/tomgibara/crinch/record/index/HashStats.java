0685dec774f78f1a4862863e3886469a2ce2164f
tomgibara <tomgibara@90b63902-e2c4-11de-9097-f71ab900b11a>
2012-01-23 21:38:59 +0000

    Hash allows choice of ordinal or position for indexing.

--- trunk/crinch/crinch-record/src/main/java/com/tomgibara/crinch/record/index/HashStats.java
+++ trunk/crinch/crinch-record/src/main/java/com/tomgibara/crinch/record/index/HashStats.java
@@ -7,6 +7,7 @@ import com.tomgibara.crinch.record.process.ProcessContext;
 
 class HashStats extends IndexStats {
 
+	boolean ordinal;
 	int tableSize;
 	int[] hashSeeds;
 	int valueBits;
@@ -17,6 +18,7 @@ class HashStats extends IndexStats {
 
 	@Override
 	public void writeTo(CodedWriter writer) {
+		writer.getWriter().writeBoolean(ordinal);
 		writer.writePositiveInt(tableSize + 1);
 		CodedStreams.writePrimitiveArray(writer, hashSeeds);
 		writer.writePositiveInt(valueBits);
@@ -24,6 +26,7 @@ class HashStats extends IndexStats {
 
 	@Override
 	public void readFrom(CodedReader reader) {
+		ordinal = reader.getReader().readBoolean();
 		tableSize = reader.readPositiveInt() - 1;
 		hashSeeds = CodedStreams.readIntArray(reader);
 		valueBits = reader.readPositiveInt();

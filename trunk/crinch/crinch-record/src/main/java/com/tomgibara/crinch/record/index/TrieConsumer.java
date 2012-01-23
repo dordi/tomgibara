31b66597a1480d76451b18278d844d140d70a655
tomgibara <tomgibara@90b63902-e2c4-11de-9097-f71ab900b11a>
2012-01-23 14:17:39 +0000

    Moved sort classes into index package.

--- trunk/crinch/crinch-record/src/main/java/com/tomgibara/crinch/record/index/TrieConsumer.java
+++ trunk/crinch/crinch-record/src/main/java/com/tomgibara/crinch/record/index/TrieConsumer.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  * 
  */
-package com.tomgibara.crinch.record.sort;
+package com.tomgibara.crinch.record.index;
 
 import java.io.BufferedOutputStream;
 import java.io.File;

567f95e4f6c590d79fb3f2dc6a7a2c3a51d43f4a
tomgibara <tomgibara@90b63902-e2c4-11de-9097-f71ab900b11a>
2012-01-23 23:56:39 +0000

    Allowing hashes to record both ordinal and position.

--- trunk/crinch/crinch-record/src/main/java/com/tomgibara/crinch/record/index/HashConsumer.java
+++ trunk/crinch/crinch-record/src/main/java/com/tomgibara/crinch/record/index/HashConsumer.java
@@ -14,7 +14,6 @@ import com.tomgibara.crinch.hashing.Murmur3_32Hash;
 import com.tomgibara.crinch.record.LinearRecord;
 import com.tomgibara.crinch.record.RecordConsumer;
 import com.tomgibara.crinch.record.RecordStats;
-import com.tomgibara.crinch.record.compact.RecordCompactor;
 import com.tomgibara.crinch.record.def.ColumnType;
 import com.tomgibara.crinch.record.def.RecordDef;
 import com.tomgibara.crinch.record.def.SubRecordDef;
@@ -31,7 +30,6 @@ import com.tomgibara.crinch.util.WriteStream;
 public class HashConsumer implements RecordConsumer<LinearRecord> {
 
 	private static final int HASH_COUNT = 3;
-	private static final int ENTRY_SIZE = HASH_COUNT + 2;
 	private static final float DEFAULT_LOAD_FACTOR = 0.85f;
 	//TODO estimate this properly!
 	private static final double AVERAGE_KEY_SIZE_IN_BYTES = 250.0;
@@ -41,25 +39,28 @@ public class HashConsumer implements RecordConsumer<LinearRecord> {
 	@SuppressWarnings("unchecked")
 	private final Murmur3_32Hash<LinearRecord>[] hashes = new Murmur3_32Hash[HASH_COUNT];
 	private final Random random = new Random();
-	private final int[] first = new int[HASH_COUNT + 2];
-	private final int[] second = new int[HASH_COUNT + 2];
 	// prepared state
 	private ProcessContext context;
 	private RecordStats recStats;
 	private BigInteger recordCount;
 	private RecordDef recordDef;
-	private RecordCompactor compactor;
 	private ClassConfig config;
 	DynamicRecordFactory factory;
 	private RecordHashSource hashSource;
 	private HashStats hashStats;
+	private int recordSize;
+	private int entrySize;
+	private int[] first;
+	private int[] second;
+
 	private File file;
 	// pass state
 	private UniquenessChecker<LinearRecord> checker;
 	float loadFactor;
 	private int[] map;
 	private boolean passAborted;
-	private long largestValue = -1L;
+	private long largestOrdinal = -1L;
+	private long largestPosition = -1L;
 	
 	public HashConsumer(SubRecordDef subRecDef) {
 		this.subRecDef = subRecDef;
@@ -89,7 +90,14 @@ public class HashConsumer implements RecordConsumer<LinearRecord> {
 		Boolean skipUniqueCheck = recordDef.getBooleanProperty("hash.skipUniqueCheck");
 		checker = skipUniqueCheck != null && skipUniqueCheck ? null : new UniquenessChecker<LinearRecord>(recordCount.longValue(), AVERAGE_KEY_SIZE_IN_BYTES, hashSource);
 		Boolean ordinal = recordDef.getBooleanProperty("hash.ordinal");
-		hashStats.ordinal = ordinal == null ? false : ordinal.booleanValue();
+		Boolean positional = recordDef.getBooleanProperty("hash.positional");
+		hashStats.ordinal = ordinal == null ? true : ordinal.booleanValue();
+		hashStats.positional = positional == null ? true : positional.booleanValue();
+		if (!hashStats.ordinal && !hashStats.positional) throw new IllegalArgumentException("definition specifies neither positional nor ordinal");
+		recordSize = hashStats.ordinal && hashStats.positional ? 4 : 2;
+		entrySize = recordSize + hashes.length;
+		first = new int[entrySize];
+		second = new int[entrySize];
 	}
 
 	@Override
@@ -113,7 +121,7 @@ public class HashConsumer implements RecordConsumer<LinearRecord> {
 			// set up stats
 			// table size
 			double tableSize = recordCount.doubleValue() / loadFactor;
-			if (tableSize > Integer.MAX_VALUE / ENTRY_SIZE) throw new IllegalStateException("large record counts not currently supported");
+			if (tableSize > Integer.MAX_VALUE / entrySize) throw new IllegalStateException("large record counts not currently supported");
 			hashStats.tableSize = (int) Math.ceil(tableSize);
 			// hash seeds
 			int[] hashSeeds = new int[HASH_COUNT];
@@ -123,9 +131,9 @@ public class HashConsumer implements RecordConsumer<LinearRecord> {
 			}
 			hashStats.hashSeeds = hashSeeds;
 			// init map
-			if (map == null || map.length != hashStats.tableSize * ENTRY_SIZE) {
+			if (map == null || map.length != hashStats.tableSize * entrySize) {
 				map = null;
-				map = new int[hashStats.tableSize * ENTRY_SIZE];
+				map = new int[hashStats.tableSize * entrySize];
 			}
 			Arrays.fill(map, -1);
 		}
@@ -140,23 +148,43 @@ public class HashConsumer implements RecordConsumer<LinearRecord> {
 		if (checker != null) {
 			passAborted = checker.add(subRec);
 		} else {
-			long value;
-			//TODO use position by default
-			if (hashStats.ordinal) {
-				value = record.getOrdinal();
-				if (value < 0L) throw new IllegalArgumentException("record without ordinal");
+			// get position
+			long position;
+			if (hashStats.positional) {
+				position = record.getPosition();
+				if (position < 0) throw new IllegalArgumentException("record without position");
+				if (largestPosition < position) largestPosition = position;
 			} else {
-				value = record.getPosition();
-				if (value < 0L) throw new IllegalArgumentException("record without position");
+				position = -1L;
 			}
-			if (largestValue < value) largestValue = value;
 			
-			first[0] = (int) (value >> 32);
-			first[1] = (int) value;
-			for (int i = 0; i < HASH_COUNT; i++) {
-				subRec.reset();
-				first[2 + i] = ENTRY_SIZE * ((hashes[i].hashAsInt(subRec) & 0x7fffffff) % (int) hashStats.tableSize);
+			// get ordinal
+			long ordinal;
+			if (hashStats.ordinal) {
+				ordinal = record.getOrdinal();
+				if (ordinal < 0) throw new IllegalArgumentException("record without ordinal");
+				if (largestOrdinal < ordinal) largestOrdinal = ordinal;
+			} else {
+				ordinal = -1L;
 			}
+			
+			// create entry
+			int i = 0;
+			if (hashStats.positional) {
+				first[i++] = (int) (position >> 32);
+				first[i++] = (int) position;
+			}
+			if (hashStats.ordinal) {
+				first[i++] = (int) (ordinal >> 32);
+				first[i++] = (int) ordinal;
+			}
+			for (int j = 0; j < HASH_COUNT; j++) {
+				subRec.reset();
+				//TODO use hash reranging
+				first[i++] = entrySize * ((hashes[j].hashAsInt(subRec) & 0x7fffffff) % (int) hashStats.tableSize);
+			}
+			
+			//put entry
 			putEntry(first, 0);
 		}
 		
@@ -169,17 +197,25 @@ public class HashConsumer implements RecordConsumer<LinearRecord> {
 			passAborted = true;
 		} else {
 			for (int i = 0; i < HASH_COUNT; i++) {
-				int index = entry[2 + i];
-				if (map[index] == -1 && map[index+1] == -1) {
-					System.arraycopy(entry, 0, map, index, ENTRY_SIZE);
+				int index = entry[recordSize + i];
+				boolean occupied = false;
+				for (int j = index + recordSize - 2; j >= index; j -= 2) {
+					if (map[j] != -1 || map[j+1] != -1) {
+						occupied = true;
+						break;
+					}
+				}
+				if (!occupied) {
+					System.arraycopy(entry, 0, map, index, entrySize);
 					return;
 				}
 			}
 			int[] other = entry == first ? second : first;
 			int i = random.nextInt(HASH_COUNT);
-			int index = entry[2 + i];
-			System.arraycopy(map, index, other, 0, ENTRY_SIZE);
-			System.arraycopy(entry, 0, map, index, ENTRY_SIZE);
+			int index = entry[recordSize + i];
+			System.arraycopy(map, index, other, 0, entrySize);
+			System.arraycopy(entry, 0, map, index, entrySize);
+			//TODO remove recursion, this may blow the stack
 			putEntry(other, attempts + 1);
 		}
 	}
@@ -199,7 +235,8 @@ public class HashConsumer implements RecordConsumer<LinearRecord> {
 		} else {
 			if (!passAborted) {
 				// record final stats info
-				hashStats.valueBits = 64 - Long.numberOfLeadingZeros(largestValue + 1);
+				hashStats.positionBits = 64 - Long.numberOfLeadingZeros(largestPosition + 1);
+				hashStats.ordinalBits = 64 - Long.numberOfLeadingZeros(largestOrdinal + 1);
 				// write the stats
 				hashStats.write();
 				// write the table
@@ -222,6 +259,12 @@ public class HashConsumer implements RecordConsumer<LinearRecord> {
 	
 	// wipe references to free memory
 	private void cleanup() {
+		recStats = null;
+		factory = null;
+		first = null;
+		second = null;
+		checker = null;
+		map = null;
 	}
 
 	private void writeFile() {
@@ -230,10 +273,20 @@ public class HashConsumer implements RecordConsumer<LinearRecord> {
 			public void writeTo(CodedWriter writer) {
 				final BitWriter w = writer.getWriter();
 				final int[] map = HashConsumer.this.map;
-				final int valueBits = hashStats.valueBits;
-				for (int i = 0; i < map.length; i += ENTRY_SIZE) {
-					long value = (((long) map[i]) << 32) | (map[i+1] & 0x00000000ffffffffL);
-					w.write(value, valueBits);
+				final HashStats stats = hashStats;
+				for (int i = 0; i < map.length; i += entrySize) {
+					int j = i;
+					//TODO could optimize by only doing int if bits <= 32
+					if (stats.positional) {
+						long v1 = ((long) map[j++]) << 32;
+						long v2 = (map[j++] & 0x00000000ffffffffL);
+						w.write(v1 | v2, hashStats.positionBits);
+					}
+					if (stats.ordinal) {
+						long v1 = ((long) map[j++]) << 32;
+						long v2 = (map[j++] & 0x00000000ffffffffL);
+						w.write(v1 | v2, hashStats.ordinalBits);
+					}
 				}
 			}
 		}, hashStats.coding, file);
--- trunk/crinch/crinch-record/src/main/java/com/tomgibara/crinch/record/index/HashStats.java
+++ trunk/crinch/crinch-record/src/main/java/com/tomgibara/crinch/record/index/HashStats.java
@@ -7,10 +7,12 @@ import com.tomgibara.crinch.record.process.ProcessContext;
 
 class HashStats extends IndexStats {
 
+	boolean positional;
 	boolean ordinal;
 	int tableSize;
 	int[] hashSeeds;
-	int valueBits;
+	int positionBits;
+	int ordinalBits;
 	
 	HashStats(ProcessContext context) {
 		super("hash", context);
@@ -18,18 +20,22 @@ class HashStats extends IndexStats {
 
 	@Override
 	public void writeTo(CodedWriter writer) {
+		writer.getWriter().writeBoolean(positional);
 		writer.getWriter().writeBoolean(ordinal);
 		writer.writePositiveInt(tableSize + 1);
 		CodedStreams.writePrimitiveArray(writer, hashSeeds);
-		writer.writePositiveInt(valueBits);
+		writer.writePositiveInt(positionBits);
+		writer.writePositiveInt(ordinalBits);
 	}
 
 	@Override
 	public void readFrom(CodedReader reader) {
+		positional = reader.getReader().readBoolean();
 		ordinal = reader.getReader().readBoolean();
 		tableSize = reader.readPositiveInt() - 1;
 		hashSeeds = CodedStreams.readIntArray(reader);
-		valueBits = reader.readPositiveInt();
+		positionBits = reader.readPositiveInt();
+		ordinalBits = reader.readPositiveInt();
 	}
 	
 

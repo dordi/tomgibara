d6d486acbc95b045401651c1be09121b0de0dc37
tomgibara <tomgibara@90b63902-e2c4-11de-9097-f71ab900b11a>
2012-02-28 22:26:15 +0000

    Progress on producing sample BitVector code.

--- trunk/crinch/crinch-bits/src/test/java/com/tomgibara/crinch/bits/BitVectorSample.java
+++ trunk/crinch/crinch-bits/src/test/java/com/tomgibara/crinch/bits/BitVectorSample.java
@@ -1,5 +1,9 @@
 package com.tomgibara.crinch.bits;
 
+import java.math.BigInteger;
+
+import com.tomgibara.crinch.bits.BitVector.Operation;
+
 import junit.framework.TestCase;
 
 public class BitVectorSample extends TestCase {
@@ -103,10 +107,14 @@ public class BitVectorSample extends TestCase {
 			/**
 			 * This is subtle, but nevertheless powerful: all of the eight bits
 			 * spanned by the byte have been ANDed and none of the other bits
-			 * are affected. Conversely, all the targeted bits must lay within
-			 * the span of the vector. Operations can apply bits from a number
-			 * of other sources, some of which will be described later.
+			 * are modified. Conversely, all the targeted bits must lay within
+			 * the span of the vector. So what if you have a number but you only
+			 * want to apply some of its bits? For this, the number of (least
+			 * significant bits) that should be modified can specified
 			 */
+
+			v.setBits(6, 12, 4); // apply 4 bits starting at index 6
+			assertEquals(new BitVector("1100010001"), v);
 
 			/**
 			 * The NOT operation is also supported, but is referred to as "flip"
@@ -115,67 +123,242 @@ public class BitVectorSample extends TestCase {
 			 */
 
 			v.flipBit(2);
-			assertEquals(new BitVector("0000010101"), v);
+			assertEquals(new BitVector("1100010101"), v);
 			v.flipBit(2);
-			assertEquals(new BitVector("0000010001"), v);
+			assertEquals(new BitVector("1100010001"), v);
 
 			/**
-			 * For convenience, it's easy to target every bit in a single operation. For
-			 * example, this will clear all bits:
+			 * For convenience, it's easy to target every bit in a single
+			 * operation. For example, this will clear all bits:
 			 */
 
 			v.set(false);
 			assertEquals(new BitVector("0000000000"), v);
-			
+
 			/**
 			 * and this will flip them all:
 			 */
-			
+
 			v.flip();
 			assertEquals(new BitVector("1111111111"), v);
-		
-			//TODO modify versions
+
+			/**
+			 * For every method that applies a bit operation, there is a
+			 * corresponding method which does the same thing, but takes the
+			 * operator as an additional parameter. The following pairs of calls
+			 * are equivalent:
+			 */
+
+			v.andBit(0, true);
+			v.modifyBit(Operation.AND, 0, true);
+
+			v.setRange(8, 10, true);
+			v.modifyRange(Operation.SET, 8, 10, true);
+
+		}
+
+		{ // NUMBERS
+
+			/**
+			 * Just as Java's integral primitives are fixed-width bit words, a
+			 * BitVector can be regarded as an integral value, but in contrast
+			 * to Java, BitVector values are always unsigned. It's easy to
+			 * create a BitVector from a number using the string constructor
+			 * which takes a radix.
+			 */
+
+			String monsterDigits = "808017424794512875886459904961710757005754368000000000";
+			BitVector v = new BitVector(monsterDigits, 10);
+
+			/**
+			 * BitVectors can also be constructed from BigIntegers.
+			 */
+
+			BigInteger monsterInteger = new BigInteger(monsterDigits);
+			BitVector w = BitVector.fromBigInteger(monsterInteger);
+
+			assertEquals(v, w);
+
+			/**
+			 * BitVectors constructed in this way will always contain the least
+			 * number of bits required to contain the number; in other words,
+			 * the most significant bit is always 1.
+			 */
+
+			assertTrue(v.getBit(v.size() - 1));
+
+			/**
+			 * Another consequence is that a BitVector constructed from the
+			 * number zero will have a size of zero.
+			 */
+
+			assertEquals(0, new BitVector("0", 10).size());
+			assertEquals(0, BitVector.fromBigInteger(BigInteger.ZERO).size());
+
+			/**
+			 * If a greater number of bits is required, the size of a BitVector
+			 * constructed from a BigInteger can be supplied as an extra
+			 * parameter:
+			 */
+
+			assertEquals(new BitVector("00000001"), BitVector.fromBigInteger(
+					BigInteger.ONE, 8));
+
+			/**
+			 * Note that, because BitVectors are unsigned, any attempt to
+			 * construct one from a negative number will fail. So far we've
+			 * dealt with converting numbers into BitVectors, of course, it's
+			 * possible to do the reverse and convert a BitVector back into a
+			 * number:
+			 */
+
+			assertEquals(monsterInteger, v.toBigInteger());
+
+			/**
+			 * Reflecting BitVector's role in storing numeric values, the class
+			 * implements the Number interface and any BitVector can be
+			 * converted into primitive values via that interface.
+			 */
+
+			assertEquals(monsterInteger.longValue(), v.longValue());
+
+			assertEquals(234, new BitVector("234", 10).intValue());
+
+			/**
+			 * As with all other implementations of Number in the core Java
+			 * packages, these methods take the least significant bits of the
+			 * BitVector and silently truncate any other bits.
+			 */
+
+			assertEquals(85, new BitVector("0101010101010101").byteValue());
+
+			/**
+			 * Note that truncation can result in negative values being returned
+			 * from these methods even though the BitVector itself always
+			 * represents a positive whole number.
+			 */
+
+			assertTrue(monsterInteger.longValue() < 0L);
+
+			/**
+			 * It is also possible to extract bits from any position in the
+			 * BitVector as a primitive value.
+			 */
+
+			assertEquals((byte) (v.longValue() >>> 56), v.getByte(56));
+
 		}
 
 		{ // SHIFTS AND ROTATIONS
-			// shift, rotate, reverse
-		}
-		
-		{ // NUMBERS
-			//BigInteger constructor
-			//xxValue methods
-		}
-		
-		{ // COPIES AND VIEWS
+
+			/**
+			 * Just as Java provides bit shift operators for ints and longs,
+			 * BitVector provides the same functionality (and more) but for
+			 * potentially much larger bit sequences. BitVector provides a
+			 * single method that handles all of Java's bit shift operators
+			 * (>>>, >> and <<). It takes two parameters, the first indicates
+			 * how far the bits should be shifted with the sign of the number
+			 * indicating the direction (negative is left, positive is right)
+			 * and the second gives the value that should used to populate the
+			 * vacated bits.
+			 */
+
+			BitVector v = new BitVector("11001010");
+			v.shift(1, true);
+			assertEquals(new BitVector("10010101"), v);
+			v.shift(-2, false);
+			assertEquals(new BitVector("00100101"), v);
+
+			/**
+			 * There is no limit to the distance that a BitVector may be
+			 * shifted. Obviously any shift distance that exceeds the size of
+			 * the BitVector will eradicate all bits.
+			 */
+
+			v.shift(8, false);
+			assertEquals(new BitVector("00000000"), v);
+
+			/**
+			 * Similar to bit shifts, BitVector can also rotate bits. Bits that
+			 * are pushed off one end populate the vacated positions at the
+			 * other.
+			 */
+
+			v = new BitVector("11001010");
+			v.rotate(1);
+			assertEquals(new BitVector("10010101"), v);
+
+			/**
+			 * As with shifting, there is no limit to the distance over which
+			 * bits can be rotated. Unlike shifting, bits are never lost.
+			 */
+
+			v = new BitVector("11001010");
+			v.rotate(v.size());
+			assertEquals(new BitVector("11001010"), v);
+
+			/**
+			 * In addition to shifts and rotations, BitVector can reverse the
+			 * order of the bits.
+			 */
+
+			v.reverse();
+			assertEquals(new BitVector("01010011"), v);
+			
+			/**
+			 * Reversing a BitVector twice will naturally restore the bits to
+			 * their original order.
+			 */
+
+			v.reverse();
+			assertEquals(new BitVector("11001010"), v);
+
+			/**
+			 * Finally, you should be aware that all of these operations
+			 * (shifting, rotating and reversing) are available over arbitrary
+			 * ranges. Here are some examples:
+			 */
+
+			v = new BitVector("11001010");
+			v.shiftRange(0, 4, 1, false);
+			assertEquals(new BitVector("11000100"), v);
+			v.rotateRange(2, 7, -1);
+			assertEquals(new BitVector("11100000"), v);
+			v.reverseRange(2, 8);
+			assertEquals(new BitVector("00011100"), v);
 			
 		}
-		
+
+		{ // COPIES AND VIEWS
+
+		}
+
 		{ // VECTOR OPERATIONS
 			// compare, testX
 		}
-		
+
 		{ // ANALYZING
 			// first, last, count, all, next
 		}
-		
+
 		{ // BYTES
 			// byte static factory method
 			// operations on byte arrays
 			// write/read methods
 		}
-		
+
 		{ // COLLECTIONS
-			//iterator, listIterator, positionIterator
-			//asList
-			//asSet
+			// iterator, listIterator, positionIterator
+			// asList
+			// asSet
 		}
-		
+
 		{ // JAVA OBJECT
 			// cloneable
 			// serializable
 			// equals, hashcode
 		}
-		
+
 		{ // EFFICIENCY
 			// alignment
 			// avoiding no-ops

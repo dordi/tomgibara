593a13b93347e75fe5dde98ef2d9aeb4ef241989
tomgibara <tomgibara@90b63902-e2c4-11de-9097-f71ab900b11a>
2012-03-07 10:14:25 +0000

    Documented FileBitReaderFactory.

--- trunk/crinch/crinch-bits/src/main/java/com/tomgibara/crinch/bits/FileBitReaderFactory.java
+++ trunk/crinch/crinch-bits/src/main/java/com/tomgibara/crinch/bits/FileBitReaderFactory.java
@@ -7,13 +7,50 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 
+/**
+ * Provides a convenient way of opening and closing {@link BitReader}s over a
+ * file. A {@link Mode} supplied to the constructor controls the characteristics
+ * of returned readers.
+ * 
+ * @author Tom Gibara
+ * 
+ */
+
 public class FileBitReaderFactory {
 
+	/**
+	 * The default size of byte buffer used to read files.
+	 */
+	
 	public static final int DEFAULT_BUFFER_SIZE = 8192;
 	
+	/**
+	 * Specifies the method by which bits are read from the underlying file.
+	 */
+	
 	public enum Mode {
+		
+		/**
+		 * The entire file is read into memory and readers over the in-memory
+		 * copy are created. This is the fastest mode of access if the file will
+		 * be read multiple times, but may incur a significant memory overhead.
+		 */
+		
 		MEMORY,
+		
+		/**
+		 * A channel to the file is obtained for each reader and bits are read
+		 * from the channel.
+		 */
+		
 		CHANNEL,
+		
+		/**
+		 * An input stream over the file is obtained for each reader and bits
+		 * are read from the input stream. This mode does not support moving the
+		 * read position backwards through the file.
+		 */
+		
 		STREAM
 	}
 
@@ -22,11 +59,38 @@ public class FileBitReaderFactory {
 	private final int bufferSize;
 	private byte[] bytes = null;
 
-	public FileBitReaderFactory(File file, Mode mode) {
+	/**
+	 * Constructs a new {@link FileBitReaderFactory} using the default buffer
+	 * size specified by {@link #DEFAULT_BUFFER_SIZE}.
+	 * 
+	 * @param file
+	 *            the file from which bits are to be read
+	 * @param mode
+	 *            the method by which bits are obtained from the file
+	 * @throws IllegalArgumentException
+	 *             if any parameter is null
+	 */
+	
+	public FileBitReaderFactory(File file, Mode mode) throws IllegalArgumentException {
 		this(file, mode, DEFAULT_BUFFER_SIZE);
 	}
 	
-	public FileBitReaderFactory(File file, Mode mode, int bufferSize) {
+	/**
+	 * Constructs a new {@link FileBitReaderFactory} using the specified buffer
+	 * size. When the mode is {@link Mode#MEMORY}, the bufferSize is ignored.
+	 * 
+	 * @param file
+	 *            the file from which bits are to be read
+	 * @param mode
+	 *            the method by which bits are obtained from the file
+	 * @param bufferSize
+	 *            the size of the buffer
+	 * @throws IllegalArgumentException
+	 *             if file is null, the mode is null, or the bufferSize is not
+	 *             positive
+	 */
+
+	public FileBitReaderFactory(File file, Mode mode, int bufferSize) throws IllegalArgumentException {
 		if (file == null) throw new IllegalArgumentException("null file");
 		if (mode == null) throw new IllegalArgumentException("null mode");
 		if (bufferSize < 1) throw new IllegalArgumentException("non-positive bufferSize");
@@ -34,18 +98,52 @@ public class FileBitReaderFactory {
 		this.mode = mode;
 		this.bufferSize = bufferSize;
 	}
+
+	/**
+	 * The file from which bits are to be read.
+	 * 
+	 * @return the file, never null
+	 */
 	
 	public File getFile() {
 		return file;
 	}
+
+	/**
+	 * The method by which bits will be read from the file.
+	 * 
+	 * @return the mode, never null
+	 */
 	
 	public Mode getMode() {
 		return mode;
 	}
+
+	/**
+	 * The size of the buffer used to read bytes from the file. The buffer size
+	 * is irrelevant when the mode is {@link Mode#MEMORY}.
+	 * 
+	 * @return the buffer size, always positive
+	 */
 	
 	public int getBufferSize() {
 		return bufferSize;
 	}
+	
+	/**
+	 * Opens a reader over the bits of the file. The characteristics of the
+	 * returned reader are determined by the {@link Mode} in which the factory
+	 * was created.
+	 * 
+	 * Any reader returned by this method SHOULD eventually be closed by passing
+	 * it to the {@link #closeReader(BitReader)} method. Not doing so may risk
+	 * leaking system resources.
+	 * 
+	 * @return a new reader over the file
+	 * @throws BitStreamException
+	 *             if the reader could not be opened, typically because the file
+	 *             could not be read
+	 */
 	
 	public BitReader openReader() throws BitStreamException {
 		try {
@@ -60,7 +158,19 @@ public class FileBitReaderFactory {
 		}
 	}
 	
-	public void closeReader(BitReader reader) throws BitStreamException {
+	/**
+	 * Closes a reader that was previously opened with a call to
+	 * {@link #openReader()}. Closing a reader multiple times has no effect.
+	 * 
+	 * @param reader
+	 *            the reader to close
+	 * @throws IllegalArgumentException
+	 *             if the supplied reader was null
+	 * @throws BitStreamException
+	 *             if an IOException was raised when closing the file
+	 */
+	
+	public void closeReader(BitReader reader) throws IllegalArgumentException, BitStreamException {
 		if (reader == null) throw new IllegalArgumentException("null reader");
 		if (reader instanceof InputStreamBitReader) {
 			try {
